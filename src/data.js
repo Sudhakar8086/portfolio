f12d1366e689ecb380f01ab8227a52441068bc51
Sudhakar Kadepaka <kadepakasudhakar8086@gmail.com>
2023-11-15 18:35:16 +0530

    Deploy

---
+++ src/data.js
@@ -0,0 +1,66 @@
+import YourImage from './images/register.jpg';
+import MyImage from './images/s365.jpeg'
+
+export const projects = [
+  {
+    title: "Corders- Business Intelligence",
+    subtitle: "MERN Stack",
+    description:
+      "I played a key role in developing a sleek business intelligence website, emphasizing modern design and optimizing performance for users navigating advanced analytics and reporting tools.",
+      image:YourImage,
+    // link: "https://www.corders.io/login",
+  },
+  {
+    title: "Service@365",
+    subtitle: "React and Node",
+    description:
+      " I played a central role in developing a website for a computer hardware service company, ensuring a smooth user experience with incorporated features for service requests and real-time updates.",
+    image: MyImage,
+    // link: "https://reactbootcamp.com",
+  },
+  // {
+  //   title: "DevChat",
+  //   subtitle: "React and Firebase",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   // image: "./project-3.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+  // {
+  //   title: "Epic Todo App",
+  //   subtitle: "React Hooks",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-4.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+];
+
+export const testimonials = [
+  // {
+  //   quote:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+  //   image: "https://randomuser.me/api/portraits/men/1.jpg",
+  //   name: "Jesse Hicks",
+  //   company: "Zoozle",
+  // },
+  // {
+  //   quote:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+  //   image: "https://randomuser.me/api/portraits/men/94.jpg",
+  //   name: "Ruben Alvarez",
+  //   company: "Dooble.io",
+  // },
+];
+
+export const skills = [
+  // "HTML", "CSS",
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "MySql",
+  "Material UI",
+  "MongoDB",
+  "AWS"
+];
